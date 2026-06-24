ae6f22b153d3f5e32cb3d5499b3b45272afb1a34
Thu Anh <jqef076@gmail.com>
2026-06-24 11:09:47 +0700

    Add service worker copy plugin to Vite config
    Added a plugin to copy sw.js to the dist directory during the build process.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import fs from 'fs'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-sw',
+      closeBundle() {
+        fs.copyFileSync(resolve(__dirname, 'sw.js'), resolve(__dirname, 'dist/sw.js'));
+      }
+    }
+  ],
   build: {
     outDir: 'dist',
     rollupOptions: {
